fdfa99909ddb8c7adde19a7e51d805d194cb8055
cepthomas <cepthomas@gmail.com>
2021-04-04 10:57:25 -0500

    Format tests.

--- test/test_common.py
+++ test/test_common.py
@@ -1,31 +1,8 @@
 import unittest
-#import unittest.mock
 from unittest.mock import MagicMock
 import sublime
-#import sublime_plugin
 import sbot
 import sbot_common
-
-
-
-# from unittest.mock import create_autospec
-#def function(a, b, c):
-#    pass
-#
-#mock_function = unittest.mock.create_autospec(function, return_value='fishy')
-#mock_function(1, 2, 3)
-#
-#mock_function.assert_called_once_with(1, 2, 3)
-##mock_function('wrong arguments')
-
-
-#from unittest.mock import MagicMock
-#thing = ProductionClass()
-#thing.method = MagicMock(return_value=3)
-#thing.method(3, 4, 5, key='value')
-
-#thing.method.assert_called_with(3, 4, 5, key='value')
-
 
 
 
@@ -42,7 +19,6 @@ class TestCommon(unittest.TestCase):
 
         view = sublime.View(600)
 
-
         sel = sublime.Selection(view.id())
         sel.add(sublime.Region(10, 20, 101))
         view.sel = MagicMock(return_value = sel)
@@ -51,18 +27,6 @@ class TestCommon(unittest.TestCase):
         evt.on_selection_modified(view)
 
         #self.assertEqual(1, 2, 'just a test test')
-
-
-# TODO-T sbot_common stuff:
-# def trace(*args, cat=None):
-# # Check for file size limit.
-# def error(info, exc):
-# def get_sel_regions(view):
-# def create_new_view(window, text):
-# def write_to_console(text):
-# def dump_view(preamble, view):
-# def wait_load_file(view, line):
-# class SbotPerfCounter(object):
 
 
 if __name__ == '__main__':
--- test/test_format.py
+++ test/test_format.py
@@ -1,14 +1,13 @@
 import sys
 import unittest
-#import unittest.mock
 from unittest.mock import MagicMock
 import sublime
-#import sublime_plugin
 import sbot_format
 
 
 
 class TestFormatJson(unittest.TestCase):
+
     def setUp(self):
         sbot_format.plugin_loaded()
 
@@ -17,39 +16,29 @@ class TestFormatJson(unittest.TestCase):
 
     def test_json_clean_comments_commas(self):
         '''The happy path.'''
-
-        view = sublime.View(601)
+        v = sublime.View(601)
 
         with open(r'.\files\test.json', 'r') as fp:
+            # The happy path.
             s = fp.read()
-
-            #sel = sublime.Selection(view.id())
-            #sel.add(sublime.Region(0, len(s), 0))
-            #view.sel = MagicMock(return_value = sel)
-
-            cmd = sbot_format.SbotFormatJsonCommand(view)
-
+            #print('000000', s)
+            cmd = sbot_format.SbotFormatJsonCommand(v)
             res = cmd._do_one_region(s)
+            self.assertEqual(res[:50], '{\n    "MarkPitch": {\n        "Original": 0,\n      ')
 
-            print(res)
-
-
-
-
-
-
-
+            # Make it a bad file.
+            s = s.replace('\"Original\"', '')
+            res = cmd._do_one_region(s)
+            self.assertEqual(res[:50], "Error: ('Expecting property name enclosed in doubl")
 
     def test_xml(self):
         '''Also has tests for the simple sbot.py.'''
-        # self.assertEqual('foo'.upper(), 'FOO')
-
         v = sublime.View(602)
 
         # class SbotFormatXmlCommand(sublime_plugin.TextCommand):
         #     def run(self, edit):
         #         # Installation: pip3 install lxml.
-        #         v = self.view
+        #         v = self.v
         #     def is_visible(self):
 
     def test_html(self):
@@ -60,7 +49,7 @@ class TestFormatJson(unittest.TestCase):
 
         # class SbotFormatHtmlCommand(sublime_plugin.TextCommand):
         #     def run(self, edit):
-        #         v = self.view
+        #         v = self.v
         #     def is_visible(self):
 
 
